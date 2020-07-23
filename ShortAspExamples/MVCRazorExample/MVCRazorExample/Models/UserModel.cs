95b88b90eceaefd516f461039e87104c1445b675
igor_spirin <igor.r10@bk.ru>
2020-07-23 03:19:29 +0300

    update MVCRazorExample

---
+++ ShortAspExamples/MVCRazorExample/MVCRazorExample/Models/UserModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace MVCRazorExample.Models
+{
+    public class UserModel
+    {
+        public bool IsLoggedIn { get; set; }
+        public string UserName { get; set; }
+    }
+}
---
+++ ShortAspExamples/MVCRazorExample/MVCRazorExample/ViewComponents/UserViewComponent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using MVCRazorExample.Models;
+
+namespace MVCRazorExample.ViewComponents
+{
+    public class UserViewComponent : ViewComponent
+    {
+        public UserViewComponent()
+        {
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = new UserModel()
+            {
+                IsLoggedIn = true,
+                UserName = "user"
+            };
+            return View("User", model);
+        }
+    }
+}
