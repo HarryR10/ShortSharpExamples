9a4e3a587f33da924c400423628e2699869d263a
igor_spirin <igor.r10@bk.ru>
2020-07-29 11:52:05 +0300

    add ASPAngularExample (template)

---
+++ ShortAspExamples/ASPAngularExample/ASPAngularExample/ClientApp/src/app/app.component.ts
@@ -0,0 +1,10 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'app',
+    template: `<input [(ngModel)]="text" placeholder="text" />
+                 <h2>Ok, {{text}}</h2>`
+})
+export class AppComponent {
+    text = '';
+}
