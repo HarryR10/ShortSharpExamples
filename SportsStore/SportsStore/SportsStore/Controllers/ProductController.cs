fdfc034e096140521f3cafb30c568f96dbc44cfb
igor_spirin <igor.r10@bk.ru>
2020-08-18 22:35:34 +0300

    init(SportsStore.sln) add new solution

---
+++ SportsStore/SportsStore/SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+
+        public ProductController(IProductRepository repo)
+        // здесь, по сути, и происходит внедрение зависимости
+        // в классе startup, в ConfigureServices() мы задали соответствие
+        // для этого интерфейса
+        {
+            repository = repo;
+        }
+
+        public ViewResult List() => View(repository.Products);
+    }
+}
---
+++ SportsStore/SportsStore/SportsStore/Startup.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SportsStore.Models;
+
+namespace SportsStore
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            //services.AddMvc();
+            services.AddMvc(o => o.EnableEndpointRouting = false);
+            services.AddTransient<IProductRepository, FakeProductRepository>();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseStatusCodePages();
+
+            app.UseStaticFiles();
+
+            //app.UseMvc();
+
+            app.UseMvc(routes => {
+                routes.MapRoute(
+                name: "default",
+                template: "{controller=Product}/{action=List}/{id?}");
+            });
+        }
+    }
+}
